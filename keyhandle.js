76de5a3d22232a46215a317ad52e9fc40ec61f88
M. Elias <57570197+lsopp@users.noreply.github.com>
2022-03-28 12:27:47 -0300

    Add files via upload

---
+++ keyhandle.js
@@ -0,0 +1,19 @@
+$("body").keydown(function(e) {
+
+  if (e.keyCode == 27) {
+    $("body").css("background-color", "rgba(27,32,40,255)");
+    $("#search-input").val('');
+    $("#search-input").blur();
+    $('#search-input').hide();
+    $("#clock").show();
+  } else if (e.keyCode == 91 || e.keyCode == 18 ||
+    e.keyCode == 17 || e.keyCode == 225 || e.keyCode == 8) {
+      return;
+  } else {
+    $("#clock").hide();
+    $('#search-input').show();
+    $("#search-input").focus();
+  }
+
+});
+
