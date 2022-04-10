fd50976d2e93963158d9e66867e554d4973b730e
axbt <whuzz@protonmail.com>
2022-04-10 14:57:48 -0300

    preix only on start of the string

--- form.js
+++ form.js
@@ -4,15 +4,15 @@ $('#search-input').keyup(function(){
     $('#search-input').blur(); 
     $('#clock').show();
     $("body").css("background-color", "rgb(27,32,40)"); 
-  } else if ($(this).val().match(/r\/+/g)) {
+  } else if ($(this).val().match(/^r\/+/g)) {
     $("body").css("background-image", "linear-gradient(135deg, rgb(255, 132, 86), rgb(255, 69, 0))");
-  } else if ($(this).val().match(/tt\/+/g)) {
+  } else if ($(this).val().match(/^tt\/+/g)) {
     $("body").css("background-image", "linear-gradient(135deg, rgb(29, 161, 242), rgb(25, 96, 143))")
-  } else if ($(this).val().match(/y\/+/g)) {
+  } else if ($(this).val().match(/^y\/+/g)) {
     $("body").css("background-image", "linear-gradient(135deg, rgb(205, 32, 31), rgb(205, 76, 31))")
-  } else if ($(this).val().match(/g\/+/g)) {
+  } else if ($(this).val().match(/^g\/+/g)) {
     $("body").css("background-image", "linear-gradient(135deg, rgb(43, 43, 43), rgb(59, 59, 59)")
-  } else if ($(this).val().match(/i\/+/g)) {
+  } else if ($(this).val().match(/^i\/+/g)) {
     $("body").css("background-image", "linear-gradient(45deg, rgb(64, 93, 230), rgb(88, 81, 219), rgb(131, 58, 180), rgb(193, 53, 132), rgb(225, 48, 108), rgb(253, 29, 29)")
   } else {
     $("body").css("background-image", "none");
@@ -42,28 +42,28 @@ $(document).ready(function(){
         // for urls without http://www or https://www
         window.open(`http://www.${s_input}`, "_self", false);
         $("#search-input").val(''); 
-      } else if (s_input.match(/r\/+/g)){
+      } else if (s_input.match(/^r\/+/g)){
         // Reddit
         window.open(`https://www.reddit.com/${s_input}`, "_self", false)
         $("#search-input").val(''); 
-      } else if (s_input.match(/tt\/+/g)) {
+      } else if (s_input.match(/^tt\/+/g)) {
         // Twitter
-        s_input_without_prefix = s_input.replace(/tt\/+/g, '')
+        s_input_without_prefix = s_input.replace(/^tt\/+/g, '')
         window.open(`https://www.twitter.com/${s_input_without_prefix}`, "_self", false)
         $("#search-input").val(''); 
-      } else if (s_input.match(/y\/+/g)) {
+      } else if (s_input.match(/^y\/+/g)) {
         // Youtube
         s_input_without_prefix = s_input.replace(/y\/+/g, '')
         window.open(`https://www.youtube.com/${s_input_without_prefix}`, "_self", false);
         $("#search-input").val(''); 
-      } else if (s_input.match(/g\/+/g)) {
+      } else if (s_input.match(/^g\/+/g)) {
         // Github
-        s_input_without_prefix = s_input.replace(/g\/+/g, '')
+        s_input_without_prefix = s_input.replace(/^g\/+/g, '')
         window.open(`https://www.github.com/${s_input_without_prefix}`, "_self", false);
         $("#search-input").val(''); 
-      } else if (s_input.match(/i\/+/g)) {
+      } else if (s_input.match(/^i\/+/g)) {
         // Instagram
-        s_input_without_prefix = s_input.replace(/i\/+/g, '')
+        s_input_without_prefix = s_input.replace(/^i\/+/g, '')
         window.open(`https://www.instagram.com/${s_input_without_prefix}`, "_self", false);
         $("#search-input").val(''); 
       } else {
